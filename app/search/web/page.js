610eba15f93773d3ea56c2f3497c26549c5f921e
Idil Dogan <idilaydindogan@gmail.com>
2023-03-26 20:25:53 +0300

    Search pages & header added

---
+++ app/search/web/page.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+import WebSearchResults from "components/WebSearchResults";
+
+const WebSearcgPage = async ({ searchParams }) => {
+	const startIndex = searchParams.start || 1;
+	const response = await fetch(
+		`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`
+	);
+
+	const data = await response.json();
+	const results = data.items;
+
+	if (!response.ok) {
+		throw new Error("Something went wrong!");
+	}
+
+	if (!results) {
+		return (
+			<div className="flex flex-col items-center justify-center pt-10">
+				<h1 className="mb-4 text-3xl">No Results Found</h1>
+				<p className="text-lg">
+					Try searching something else or go back to homepage{" "}
+					<Link className="text-blue-500" href="/">
+						Home
+					</Link>
+				</p>
+			</div>
+		);
+	}
+	return <>{results && <WebSearchResults results={data} />}</>;
+};
+
+export default WebSearcgPage;
---
+++ components/SearchHeader.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import SearchBox from "./SearchBox";
+import { RiSettings3Line } from "react-icons/ri";
+import { TbGridDots } from "react-icons/tb";
+import SearchHeaderOptions from "./SearchHeaderOptions";
+
+const SearchHeader = () => {
+	return (
+		<header className="sticky top-0 bg-white">
+			<div className="flex items-center justify-between w-full p-6">
+				<Link href={"/"}>
+					<Image
+						src={"/Google.png"}
+						width={120}
+						height="40"
+						alt="google logo"
+					/>
+				</Link>
+				<div className="flex-1">
+					<SearchBox />
+				</div>
+				<div className="hidden space-x-2 text-gray-600 md:inline-flex">
+					<RiSettings3Line className="header-icon" />
+					<TbGridDots className="header-icon" />
+				</div>
+				<button className="px-6 py-2 ml-2 font-medium text-white transition-all bg-blue-500 rounded-md hover:brightness-105 hover:shadow-md">
+					Sign In
+				</button>
+			</div>
+			<SearchHeaderOptions />
+		</header>
+	);
+};
+
+export default SearchHeader;
